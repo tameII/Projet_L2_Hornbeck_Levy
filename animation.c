7a510568f5e416a687e23bbe214467e3330a142e
BrandonHornbeck <31899755+BrandonHornbeck@users.noreply.github.com>
2017-10-12 13:12:31 +0200

    animation.c
    this file runs the animation process

---
+++ animation.c
@@ -0,0 +1,18 @@
+/*Title: animation.c*/
+/*Authors: Brandon Hornbeck */
+/*                &         */
+/*         Mathieu Levy     */ 
+
+/*This is the file which is running the animation process*/
+
+
+/*  position is the pointer of the SDL_Rect which determines the current sprite used, and nbSprite is the number of 
+ *sprites in the animation and spriteSize is the size of one sprite*/
+
+void animSprite ( SDL_Rect * position, int nbSprite, int spriteSize)
+{
+  position->x += spriteSize;
+  if (position->x == nb_sprite * spriteSize){
+    position->x = 0;
+  }
+}
